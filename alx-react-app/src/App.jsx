4450ce7e4ea19bb7b092af6dd0197893b2b61485
Mia06-coder <miamudzingwa@gmail.com>
2025-08-26 20:58:10 +0200

    feat: create Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,20 +1,14 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
